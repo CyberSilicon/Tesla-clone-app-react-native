4a891ef8b182d4a54b23ac08856407becf482b30
lass <hocinehoho53@gmail.com>
2021-10-14 09:11:33 +0100

    homescreen

---
+++ components/carItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, View, Text, ImageBackground } from 'react-native'
+import styles from './styles';
+
+const CarItem = () => {
+    return (
+
+        <View style = { styles.carContainer } >
+
+            <ImageBackground 
+                source = {require('../../assets/images/ModelX.jpeg')}
+                style = {styles.image}
+            />
+            <View style = { styles.titles } >
+                <Text style = { styles.title } > Model S </Text> 
+                <Text style = { styles.subtitle } > Starting at $69 .729 </Text>
+            </View>
+    </View>
+    
+    )
+}
+
+export default CarItem;
